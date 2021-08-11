cf2e8b5466314c2e330d7ac426135efd385ee150
Lucas Oliveira Sousa Dias Rosendo <lucasrosendo91@gmail.com>
2021-08-11 01:38:56 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages/index';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ MovieList } />
-        <Route exact path="/movies/new" component={ NewMovie } />
-        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
         <Route exact path="/movies/:id/edit" component={ EditMovie } />
-        <Route exact path="*" component={ NotFound } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="*" component={ NotFound } />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
-
 export default App;
